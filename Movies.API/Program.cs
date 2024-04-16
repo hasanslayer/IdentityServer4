87e8aeb99cc906aa4638ba5e896c7f04126a04f8
hasanslayer <hasan-dr2010@hotmail.com>
2024-04-16 06:16:41 +0200

    Add project files.

---
+++ Movies.API/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Movies.API.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MoviesAPIContext>(options =>
+    options.UseInMemoryDatabase("Movies"));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+
+app.Run();
